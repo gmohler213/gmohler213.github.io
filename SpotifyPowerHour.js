b78af8af6b243754a5040f0eeedecdd6965c15ae
Greg Mohler <gmohler213@gmail.com>
2018-12-27 16:55:25 -0500

    fixed number generator, added iframe creation to power hour page

--- SpotifyPowerHour.js
+++ SpotifyPowerHour.js
@@ -2,12 +2,16 @@ var URI = prompt("Paste your public Spotify playlist's URI here:");
 var splitURI = URI.split(":");
 var userID = splitURI[2];
 var playlistID = splitURI[4];
+
 console.log(userID);
 console.log(playlistID)
-// var result;
-// const Http = new XMLHttpRequest();
-// const url = 'https://www.random.org/integers/?num=1&min=1&max=' + num + '&col=1&base=10&format=plain&rnd=new';
-// Http.open("GET", url);
-// Http.send();
-// <iframe src="https://open.spotify.com/embed/user/1267770186/playlist/4hjLI2C31D3ByLeX9kq2PR" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-// }
+
+var playerURI = "https://open.spotify.com/embed/user/" + userID + "/playlist/" + playlistID
+
+var ifrm = document.createElement('iframe');
+
+ifrm.setAttribute('id', 'spotifyPlayer');
+
+document.body.appendChild(ifrm);
+
+ifrm.setAttribute('src', playerURI);
