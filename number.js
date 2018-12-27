da6a047b728d257274ed6fc711d459d7879b36d5
Greg Mohler <gmohler213@gmail.com>
2018-12-27 16:02:29 -0500

    changed random number to window alert

--- number.js
+++ number.js
@@ -8,9 +8,11 @@ else {
 	const url = 'https://www.random.org/integers/?num=1&min=1&max=' + num + '&col=1&base=10&format=plain&rnd=new';
 	Http.open("GET", url);
 	Http.send();
+	window.alert(answer);
 	Http.onreadystatechange=function(){
 		if(this.readyState==4 && this.status==200){
-			document.getElementById("answer").innerHTML = Http.responseText;
+			// document.getElementById("answer").innerHTML = Http.responseText;
+			window.alert(Http.responseText);
 		}
 	}
 }
